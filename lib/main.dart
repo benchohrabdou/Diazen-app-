e21cdbb4b7f155d48f148616f73ab73467c79728
Abdou <abdoubench236@gmail.com>
2025-04-15 00:40:47 +0100

    added authentication and apploaded the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
-import 'package:diazen/authentication/welcomepage.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'authentication/welcomepage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-    debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       home: WelcomePage(),
     );
   }
