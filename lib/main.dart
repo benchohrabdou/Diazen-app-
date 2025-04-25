0d2d0544c57f46c458c8ad6ca572984b0f471085
Abdou <abdoubench236@gmail.com>
2025-04-25 01:11:40 +0100

    added the keep me logged in functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'authentication/welcomepage.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
-}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
